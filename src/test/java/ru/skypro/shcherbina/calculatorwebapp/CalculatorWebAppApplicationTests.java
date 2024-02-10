706330f22b937083508379deea1b1dc54e7745ce
Nikolai <kol-7787@mail.ru>
2024-02-10 12:39:55 +0300

    first commit

---
+++ src/test/java/ru/skypro/shcherbina/calculatorwebapp/CalculatorWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.shcherbina.calculatorwebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
